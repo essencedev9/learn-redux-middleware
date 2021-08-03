d818cdb54757671393ba3df2d32de2b48b6ccc3c
essencedev9 <essencedev9@nntuple.com>
2021-08-03 10:34:27 +0900

    Add API - thunk로 프로미스 다루기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+import { Route } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
 
 function App() {
-  return <CounterContainer />;
+  return (
+    <div>
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
+    </div>
+  );
 }
 
 export default App;
